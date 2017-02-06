f17274d3b8ad290ecbcbbee0ab2fcf5eda812eee
jainisenough <sonu_theflirtyguy20@yahoo.co.in>
2017-02-06 08:36:29 +0530

    Townhouse Client Data

--- server/boot/root.js
+++ server/boot/root.js
@@ -2,7 +2,6 @@
 module.exports = function (server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  var status = server.loopback.status();
   if (process.env.NODE_ENV === 'production') {
     router.use(function (req, res, next) {
       res.setHeader('Expires', new Date(Date.now() + server.get('maxAge')).toUTCString());
@@ -11,10 +10,7 @@ module.exports = function (server) {
   }
 
   router.get(/^((?!\.).)*$/, function (req, res) {
-    if (process.env.NODE_ENV === 'production')
-      status(req, res);
-    else
-      res.sendFile('/index.html', {root: __dirname + '/../../client/views'});
+    res.sendFile('/index.html', {root: __dirname + '/../../client/views'});
   });
 
   server.use(router);
